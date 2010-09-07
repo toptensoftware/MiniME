22a8819553b0b8e7c74a7766dd58e68ae97fb47e
Brad Robinson <brobinson@toptensoftware.com>
2010-09-07 17:28:45 +1000

    Added disclaimer comments

--- mm/Program.cs
+++ mm/Program.cs
@@ -1,14 +1,21 @@
-﻿using System;
+﻿// 
+//   MiniME - http://www.toptensoftware.com/minime
+// 
+//   The contents of this file are subject to the license terms as 
+//	 specified at the web address above.
+//  
+//   Software distributed under the License is distributed on an 
+//   "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+//   implied. See the License for the specific language governing
+//   rights and limitations under the License.
+// 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Reflection;
 namespace mm
 {
-
-	// "Z:\mobilize\www\mobSite\js\jQuery-1.4.2.js" -o:"Z:\mobilize\www\mobSite\js\jQuery-1.4.2.mm.js" -diag-formatted
-	// input.js -stdout -diag-ast
-
 	class Program
 	{
 		void Run(string[] args)
