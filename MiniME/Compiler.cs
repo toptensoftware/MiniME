afadf976e826acd678f80f7ae0222fabdb8a7f12
Gabe Williamson <gabe@stonefintech.com>
2012-04-30 13:13:24 -0500

    Added a bunch of xmldoc comments to Compiler.cs

--- MiniME/Compiler.cs
+++ MiniME/Compiler.cs
@@ -47,87 +47,106 @@ namespace MiniME
 		List<string> m_ResponseFiles = new List<string>();
 		List<string> m_IncludedFiles = new List<string>();
 
-		// Maximum line length before wrap
-		//  - set to zero for no line breaks
-		//  - no guarantees, long strings won't be broken
-		//    to enforce this, some operators may overhang
-		//    by a character or two.
+		/// <summary>
+    /// Maximum line length before wrap
+    ///  - set to zero for no line breaks
+    ///  - no guarantees, long strings won't be broken
+    ///    to enforce this, some operators may overhang
+    ///    by a character or two.
+		/// </summary>
 		public int MaxLineLength
 		{
 			get;
 			set;
 		}
 
-		// Enable/disable obfuscation of local symbols inside
-		// function closures
+    /// <summary>
+    /// Enable/disable obfuscation of local symbols inside function closures
+    /// </summary>
 		public bool NoObfuscate
 		{
 			get;
 			set;
 		}
 
-		// Enable/disable replacement of consts variables
+		/// <summary>
+    ///  Enable/disable replacement of consts variables
+		/// </summary>
 		public bool DetectConsts
 		{
 			get;
 			set;
 		}
 
-		// Enable/disable formatted output
-		//  - very rough formatting, just enough to be vaguely readable
-		//    for diagnostic purposes
+    /// <summary>
+    /// Enable/disable formatted output very rough formatting, just 
+    /// enough to be vaguely readable for diagnostic purposes
+    /// </summary>
 		public bool Formatted
 		{
 			get;
 			set;
 		}
 
-		// Set to include diagnostic information about symbol obfuscation
+		/// <summary>
+    ///  Set to include diagnostic information about symbol obfuscation
+		/// </summary>
 		public bool SymbolInfo
 		{
 			get;
 			set;
 		}
 
-		// Set to dump the abstract syntax tree to stdout
+		/// <summary>
+    ///  Set to dump the abstract syntax tree to stdout
+		/// </summary>
 		public bool DumpAST
 		{
 			get;
 			set;
 		}
 
-		// Set to dump scope information about all function scopes to stdout
+		/// <summary>
+    /// Set to dump scope information about all function scopes to stdout
+		/// </summary>
 		public bool DumpScopes
 		{
 			get;
 			set;
 		}
 
-		// Set to an encoding for the output file
-		//  - defaults to the same encoding as the first input file
+    /// <summary>
+    /// Set to an encoding for the output file - defaults to the same encoding 
+    /// as the first input file
+    /// </summary>
 		public Encoding OutputEncoding
 		{
 			get;
 			set;
 		}
 
-		// Set the output file name
-		//  - defaults to the name of the input file with `.js` removed
-		//    and `.js.min` appended
+    /// <summary>
+    /// Set the output file name defaults to the name of the input file 
+    /// with `.js` removed and `.js.min` appended
+    /// </summary>
 		public string OutputFileName
 		{
 			get;
 			set;
 		}
 
-		// Write to stdout instead of output file
+		/// <summary>
+    ///  Write to stdout instead of output file
+		/// </summary>
 		public bool StdOut
 		{
 			get;
 			set;
 		}
 
-		// When true, don't include the "Minified by MiniME" credit comment
+		/// <summary>
+    ///  When true, doesn't include the "Minified by MiniME" credit comment
+		/// </summary>
 		public bool NoCredit
 		{
 			get;
@@ -148,6 +167,11 @@ namespace MiniME
 			set;
 		}
 
+    /// <summary>
+    /// Get the supported option parameters and their default values.
+    /// </summary>
+    /// <param name="bWithIncludedFiles"></param>
+    /// <returns>Supported option parameters and their default values</returns>
 		public string CaptureOptions(bool bWithIncludedFiles)
 		{
 			var buf = new StringBuilder();
@@ -185,7 +209,9 @@ namespace MiniME
 		}
 
 
-		// Reset this compiler
+		/// <summary>
+		/// Clears all files.
+		/// </summary>
 		public void Reset()
 		{
 			m_files.Clear();
@@ -250,13 +276,23 @@ namespace MiniME
 			}
 		}
 
-		// Add a file to be processed
+		/// <summary>
+    /// Add a file to be processed
+		/// </summary>
+		/// <param name="strFileName">The file path</param>
+		/// <param name="Warnings">Enable lint warnings</param>
 		public void AddFile(string strFileName, bool Warnings)
 		{
 			AddFile(strFileName, null, Warnings);
 		}
 
-		public void AddFile(string strFileName, System.Text.Encoding Encoding, bool Warnings)
+	  /// <summary>
+	  /// Add a file to be processed
+	  /// </summary>
+	  /// <param name="strFileName">The file path</param>
+    /// <param name="Encoding">The encoding. Defaults to an encoding detection algorithm, and then <see cref="Encoding.UTF8"/> </param>
+	  /// <param name="Warnings">Enable lint warnings</param>
+	  public void AddFile(string strFileName, System.Text.Encoding Encoding, bool Warnings)
 		{
 			// Work out auto file encoding
 			if (Encoding == null)
@@ -269,7 +305,6 @@ namespace MiniME
 			// Use same encoding for output
 			if (OutputEncoding != null)
 				OutputEncoding = Encoding;
-
 			else
 			{
 				Encoding = Encoding.UTF8;
@@ -293,7 +328,12 @@ namespace MiniME
 			m_files.Add(i);
 		}
 
-		// Add Javascript code to be processed, direct from string
+    /// <summary>
+    /// Add javascript to be processed.
+    /// </summary>
+    /// <param name="strName">What you want to call the script.</param>
+    /// <param name="strScript">The script contents. </param>
+    /// <param name="Warnings">Enable lint warnings</param>
 		public void AddScript(string strName, string strScript, bool Warnings)
 		{
 			var i = new FileInfo();
@@ -304,19 +344,19 @@ namespace MiniME
 			m_files.Add(i);
 		}
 
+    /// <summary>
+    /// Compile all loaded scripts to a string. Can only compile EITHER JS or CSS, not both.
+    /// </summary>
+    /// <returns>The compiled scripts or styles.</returns>
 		public string CompileToString()
-		{
-			if (MinifyKind == MinifyKind.CSS)
-			{
-				return CompileCssToString();
-			}
-			else
-			{
-				return CompileJavascriptToString();
-			}
-		}
+    {
+      return MinifyKind == MinifyKind.CSS ? CompileCssToString() : CompileJavascriptToString();
+    }
 
-		// Compile all loaded script to a string
+    /// <summary>
+    /// Compile all loaded scripts to a string, using the css minifier.
+    /// </summary>
+    /// <returns>The compiled styles.</returns>
 		public string CompileCssToString()
 		{
 			// Step 1, concatenate all files
@@ -335,7 +375,10 @@ namespace MiniME
 			return minified;
 		}
 
-		// Compile all loaded script to a string
+		/// <summary>
+		/// Parses all loaded scripts as javascript and compiles them to a string.
+		/// </summary>
+		/// <returns>A string containing the minified javascript.</returns>
 		public string CompileJavascriptToString()
 		{
 			// Create a symbol allocator
@@ -474,7 +517,9 @@ namespace MiniME
 			return strResult;
 		}
 
-		// Compile all loaded files and write to the output file
+		/// <summary>
+    ///  Compile all loaded files and write to the output file using default options
+		/// </summary>
 		public void Compile()
 		{
 			// Automatic output filename
